1a78aeb4b4323379f83c25da8f20664f9bc46371
nciampelletti <natashat@shaw.ca>
2022-11-07 21:20:42 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NCBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
